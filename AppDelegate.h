cfcb38e71197a01cd24512f9f57b1b1eb06cf0a0
pragya5 <pragya.singh.mk@gmail.com>
2014-04-19 18:12:26 -0700

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  matchingame
+//
+//  Created by Pragya Singh on 4/18/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
